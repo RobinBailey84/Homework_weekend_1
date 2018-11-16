1529c36f79a2ff04724dd7af7d71f7f7c8d6994d
RobinBailey84 <robinbailey52@hotmail.com>
2018-11-16 11:33:01 +0000

    first commit second file

---
+++ pet_shop.rb
@@ -0,0 +1,11 @@
+def pet_shop_name(shop)
+return shop[:name]
+end
+
+def total_cash(shop)
+return shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(shop, sale_price)
+  shop[:admin][:total_cash] += sale_price
+end
